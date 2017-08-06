bfa78571fe2518db6aa273751502b5d4a0abd7ed
Marlon Smith <msmith@kcura.com>
2017-08-06 11:42:24 -0500

    Added Custom Attributes

---
+++ Source/SocialMedia/SocialMedia.Helpers/Attributes/MappedField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Helpers.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MappedField : Attribute
+    {
+        private string _displayName;
+        private string _fieldIdentifier;
+
+        public MappedField(String displayName, string fieldIdentifier)
+        {
+            _displayName = displayName;
+            _fieldIdentifier = fieldIdentifier;
+        }
+    }
+}
