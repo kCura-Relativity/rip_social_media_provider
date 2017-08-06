7e1c27b318665c5f1e78361e92163b62752896d8
Marlon Smith <msmith@kcura.com>
2017-08-06 11:16:38 -0500

    Final Project Setup

--- Source/SocialMedia/SocialMedia.Web/Controllers/ProviderController.cs
+++ Source/SocialMedia/SocialMedia.Web/Controllers/ProviderController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using kCura.Relativity.Client;
+using Relativity.API;
 
 namespace SocialMedia.Web.Controllers
 {
@@ -11,7 +13,25 @@ namespace SocialMedia.Web.Controllers
         // GET: Provider
         public ActionResult Index()
         {
-            return View();
+            var logger = Relativity.CustomPages.ConnectionHelper.Helper().GetLoggerFactory().GetLogger();
+            try
+            {
+                using (var rsapiClient = Relativity.CustomPages.ConnectionHelper.Helper().GetServicesManager().CreateProxy<IRSAPIClient>(ExecutionIdentity.System))
+                {
+                    var randomRDOArtifactID = 123123;
+                    rsapiClient.APIOptions = new APIOptions()
+                    {
+                        WorkspaceID = Relativity.CustomPages.ConnectionHelper.Helper().GetActiveCaseID()
+                    };
+                    var result = rsapiClient.Repositories.RDO.Read(randomRDOArtifactID);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occured while loaded the custom page");
+            }
+            
+                return View();
         }
     }
 }
---
+++ Source/SocialMedia/SocialMedia.Web/Scripts/SocialMediaProvider.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    //Create a new communication object that talks to the host page.
+    var message = IP.frameMessaging();
+
+    var getModel = function () {
+        var model = {
+            exampleViewField1: $('#exampleViewField1').val(),
+            exampleViewField2: $('#exampleViewField2').val(),
+            exampleViewField3: $('#exampleViewField3').val(),
+            exampleViewField4: $('#exampleViewField4').val(),
+            exampleViewField5: $('#exampleViewField5').val()
+        }
+        return model;
+    };
+
+    //An event raised when the user has clicked the Next or Save button.
+    message.subscribe('submit', function () {
+        //Execute save logic that persists the state.
+        var localModel = getModel();
+        this.publish("saveState", localModel);
+        //Communicate to the host page that it to continue.
+        this.publish('saveComplete', localModel);
+    });
+
+    //An event raised when a user clicks the Back button.
+    message.subscribe('back', function () {
+        //Execute save logic that persists the state.
+        this.publish('saveState', getModel());
+    });
+
+    //An event raised when the host page has loaded the current settings page.
+    message.subscribe('load', function (model) {
+        $('#exampleViewField1').val(model.exampleViewField1);
+        $('#exampleViewField2').val(model.exampleViewField2);
+        $('#exampleViewField3').val(model.exampleViewField3);
+        $('#exampleViewField4').val(model.exampleViewField4);
+        $('#exampleViewField5').val(model.exampleViewField5);
+    });
+});
