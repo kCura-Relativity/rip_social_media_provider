f188bdb4d88d2bf0d74f916ff221b4e5da3850ab
Marlon Smith <msmith@kcura.com>
2017-08-09 10:16:02 -0500

    Added field names to Twitter constants class

---
+++ Source/SocialMedia/SocialMedia.Twitter/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Twitter
+{
+    public class Constants
+    {
+        public class FieldNames
+        {
+            public const String ID = "ID";
+            public const String TwitterURL = "TwitterURL";
+            public const String TwitterHandle = "TwitterHandle";
+            public const String Text = "Text";
+            public const String DateCreated = "DateCreated";
+            public const String HashTags = "HashTags";
+            public const String HasMedia = "HasMedia";
+            public const String MediaType = "MediaType";
+            public const String MediaURL = "MediaURL";
+            public const String IsReplyTo = "IsReplyTo";
+            public const String IsReTweet = "IsReTweet";
+            public const String ReTweetCount = "ReTweetCount";
+            public const String LikeCount = "LikeCount";
+        }
+    }
+}
